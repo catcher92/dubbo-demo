7154652be2181019c37d71fefc7c165af7d3b219
caoxuedong <catcher92@163.com>
2017-02-15 15:49:44 +0800

    添加数据库常量

---
+++ servicedemo/src/main/java/com/catcher92/demo/framework/constant/DBConstant.java
@@ -0,0 +1,43 @@
+package com.catcher92.demo.framework.constant;
+
+/**
+ * Created by caoxuedong on 2017/2/15.
+ */
+public class DBConstant {
+
+    /**
+     * 数据库常量 是
+     * */
+    public static final int COMMONS_STATUS_YES = 1;
+    /**
+     * 数据库常量 否
+     * */
+    public static final int COMMONS_STATUS_NO = 0;
+
+    /**
+     * 删除状态 已删除
+     * */
+    public static final int COMMONS_DELETE_STATUS_YES = 1;
+    /**
+     * 删除状态 未删除
+     * */
+    public static final int COMMONS_DELETE_STATUS_NO = 0;
+    /**
+     * 删除状态 默认状态
+     * */
+    public static final int COMMONS_DELETE_STATUS_DEFAULT = COMMONS_DELETE_STATUS_NO;
+
+    /**
+     * 锁定状态 已锁定
+     * */
+    public static final int COMMONS_LOCK_STATUS_YES = 1;
+    /**
+     * 锁定状态 未锁定
+     * */
+    public static final int COMMONS_LOCK_STATUS_NO = 0;
+
+    /**
+     * 锁定状态 默认锁定状态
+     * */
+    public static final int COMMONS_LOCK_STATUS_DEFAULT = COMMONS_LOCK_STATUS_NO;
+}
